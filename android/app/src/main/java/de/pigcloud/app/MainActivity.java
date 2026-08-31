a7d52b00b34e20a4568b96ebcc19b9892618e6ca
PigTech <noreply@pigtech.de>
2026-08-31 17:31:53 +0000

    Source snapshot v1.0.1

--- android/app/src/main/java/de/pigcloud/app/MainActivity.java
+++ android/app/src/main/java/de/pigcloud/app/MainActivity.java
@@ -1,6 +1,8 @@
 package de.pigcloud.app;
 
 import android.os.Bundle;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,5 +11,16 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(MediaSaverPlugin.class);
         super.onCreate(savedInstanceState);
+        enableWebAuthentication();
+    }
+
+    private void enableWebAuthentication() {
+        if (!WebViewFeature.isFeatureSupported(WebViewFeature.WEB_AUTHENTICATION)) {
+            return;
+        }
+        WebSettingsCompat.setWebAuthenticationSupport(
+            getBridge().getWebView().getSettings(),
+            WebSettingsCompat.WEB_AUTHENTICATION_SUPPORT_FOR_APP
+        );
     }
 }
